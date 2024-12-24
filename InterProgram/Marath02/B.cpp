e9f6da897685de01653c1a9efda5d9b3b9114393
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-12-24 09:47:58 +0600

    Auto-commit: Tue Dec 24 09:47:56 AM +06 2024

--- InterProgram/Marath02/B.cpp
+++ InterProgram/Marath02/B.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #define fast_io                                                                \
   std::ios_base::sync_with_stdio(false);                                       \
   std::cin.tie(nullptr);
 
 using str = std::string;
-using vs = std::vector<std::string>;
-
-const vs prob = {"1/6", "1/3", "1/2", "2/3", "5/6", "1"};
 
 int main() {
-  fast_io;
-  int y, w;
-  std::cin >> y >> w;
-  int req = 6 - (y > w ? y : w);
-  std::cout << prob[req] << '\n';
+  fast_io str cmd1, cmd2;
+  std::cin >> cmd1 >> cmd2;
+
   return 0;
 }
